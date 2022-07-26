cccc5c906b51151959391720d1135deefc0d05b1
Pawel Kolacinski <pawel.kolacinski@gmail.com>
2022-07-26 15:34:35 +0200

    Add word list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import './App.css';
 import Keyboard from './components/Keyboard';
 import LetterGrid from './components/LetterGrid';
+import WordsObject from './data/words_en.json'
 
 function App() {
   console.log('App rendered')
   const [userInput, setUserInput] = useState([])
-  
-
   const keyPressHandler = useCallback(({key}) => {
     console.log('Pressed ' + key)
     if(/^[a-z]$/i.test(key)) setUserInput(state => [...state, {text: key }])
@@ -15,9 +14,24 @@ function App() {
   },[])
 
 
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const [wordToGuess, setWordToGuess] = useState('')
+  useEffect(() => {
+    setWordToGuess(WordsObject.words[getRandomInt(0,WordsObject.words.length)])
+  },[])
+  
+
+
 
   return (
     <div className="App">
+
+      <h1 className="answer">Todays word : {wordToGuess}</h1>
       
       <LetterGrid data={userInput} 
       />
