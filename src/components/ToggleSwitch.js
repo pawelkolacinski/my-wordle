9cf46a16a7f78f99c6dfd29d10558d76a4e8f6e4
Pawel Kolacinski <pawel.kolacinski@gmail.com>
2022-08-27 18:20:18 +0200

    Improved Switch accessibility

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -3,7 +3,17 @@ import './ToggleSwitch.css'
 export default function ToggleSwitch({ onClickHandler }) {
   return (
     <label className="switch">
-      <input type="checkbox" onClick={onClickHandler} />
+      <input
+        type="checkbox"
+        onClick={onClickHandler}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onClickHandler()
+            e.target.checked = !e.target.checked
+          }
+        }}
+        tabIndex="1"
+      />
       <span className="slider round"></span>
     </label>
   )
