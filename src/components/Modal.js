2e0ee4971c23281507cdad2fb8cd20510cfff737
Pawel Kolacinski <pawel.kolacinski@gmail.com>
2022-08-27 17:51:28 +0200

    Upgraded modal accessibility

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react'
 import './Modal.css'
 
 export default function Modal({ children, onCloseHandler }) {
+  const [showModal, setShowModal] = useState(true)
+
+  useEffect(() => {
+    const handleEscPress = (e) => {
+      if (e.key === 'Escape') {
+        onCloseHandler()
+        setShowModal(false)
+      }
+    }
+    document.querySelector('body').addEventListener('keydown', handleEscPress)
+
+    return () => {
+      document
+        .querySelector('body')
+        .removeEventListener('keydown', handleEscPress)
+    }
+  }, [onCloseHandler])
+
   return (
-    <div className="modal" onClick={onCloseHandler}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <span className="modal-close" onClick={onCloseHandler}>
-          &times;
-        </span>
-        <div className="modal-text">{children}</div>
+    showModal && (
+      <div className="modal" onClick={onCloseHandler}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <span className="modal-close" onClick={onCloseHandler}>
+            &times;
+          </span>
+          <div className="modal-text">{children}</div>
+        </div>
       </div>
-    </div>
+    )
   )
 }
